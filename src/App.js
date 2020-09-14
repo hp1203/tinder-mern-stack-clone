49f5488aa46c53ac6eb643463929600e0412b4b9
Himanshu Purohit <himanshu1203@gmail.com>
2020-09-15 00:50:40 +0530

    init header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Tinder Cards */}
+
+      {/* Swipe Buttons */}
     </div>
   );
 }
